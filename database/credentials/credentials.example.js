d7cabea12d6e3277ba13f2b970774fc6a6aec2ac
arsimr16 <arsimr16@g.holycross.edu>
2017-10-10 14:06:47 -0700

    create credentials.example.js file and add credentials.js to gitignore

---
+++ database/credentials/credentials.example.js
@@ -0,0 +1,10 @@
+// Create a copy of this file in the same directory named 'credentials.js'.
+// Store your credentials for connecting to postgreSQL.
+// 'credentials.js' will automatically be ignored by git
+
+const credentials = {
+	username: '',
+	password: ''
+};
+
+module.exports = credentials;
