82a68e60979817602f9dee7ef2bef43b3be34af1
arsimr16 <arsimr16@g.holycross.edu>
2017-10-11 10:44:45 -0700

    still not working

--- server/yelp.js
+++ server/yelp.js
@@ -1,11 +1,12 @@
 const db = require('../database/index.js');
 const yelpAPIKey = require('./credentials/credentials.js');
-const jquery = require('jquery');
+const $ = require('jquery');
 
+console.log('JQUERY: ', $);
 const yelp = {
 	get: (req, res, paramObj) => {
-		jquery.ajax({
-			url: 'https://api.yelp.com/v3/businesses/search?' + jquery.param({term: 'pizza', location: 'san francisco'}),
+		$.ajax({
+			url: 'https://api.yelp.com/v3/businesses/search?' + $.param({term: 'pizza', location: 'san francisco'}),
 			method: 'GET',
 			headers: {'Authorization': 'Bearer ' + yelpAPIKey.access_token},
 			success: (restaurants) => {
