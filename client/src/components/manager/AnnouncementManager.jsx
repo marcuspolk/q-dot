fe8fde2c87299db9e2dd962e968775f46a30b6a4
Marcus Polk <marcuspolk96@gmail.com>
2017-10-11 21:48:22 -0700

    added modal for announcement as well as some helper functions

--- client/src/components/manager/AnnouncementManager.jsx
+++ client/src/components/manager/AnnouncementManager.jsx
@@ -4,8 +4,36 @@ class AnnouncementManager extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      announcements: []
+      announcements: [],
+      modalAnnouncement: null
     };
+  }
+
+  updateStatus() {
+
+  }
+
+  populateModal(announcement) {
+    // if announcement present, state is edit.
+    if (announcement) {
+      this.setState(
+        {modalAnnouncement: announcement}
+      );
+    } else {
+    // if not, start a new announcement, setState
+      this.setState(
+        {modalAnnouncement: null}
+      );
+      console.log('no announcement or something');
+    }
+  }
+
+  submitAnnouncement() {
+
+  }
+
+  delete() {
+
   }
 
   componentDidMount() {
@@ -26,34 +54,54 @@ class AnnouncementManager extends React.Component {
 
   render() {
     return (
-            <div className="panel panel-default">
-              <div className="table-responsive">
-                <table className="table table-striped">
-                  <thead>
-                    <tr>
-                      <th>#</th>
-                      <th>announcement</th>
-                      <th>status</th>
-                      <th>time</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {this.state.announcements.map(announcement => {
-                      return (
-                        <tr key={announcement.id}>
-                          <td>{announcement.id}</td>
-                          <td>{announcement.message}</td>
-                          <td>{announcement.status}</td>
-                          <td>{announcement.updatedAt}</td>
-                          <td>edit</td>
-                          <td>deactivate</td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
+      <div>
+        <div id="announcement-editor" className="modal fade" role="dialog">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal">&times;</button>
+                <h3 className="modal-title">Announcement Editor</h3>
               </div>
-            </div>);
+              <div className="modal-body">
+                <p className="warning-content">FORM GOES HERE</p>
+              </div>
+              <div className="modal-footer">
+                <button className="btn btn-success" data-dismiss="modal" onClick={() => console.log('put something here')}>Submit</button>
+                <button type="button" className="btn btn-default" data-dismiss="modal">Cancel</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="panel panel-default">
+          <div className="table-responsive">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>announcement</th>
+                  <th>status</th>
+                  <th>time</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.announcements.map(announcement => {
+                  return (
+                    <tr key={announcement.id}>
+                      <td>{announcement.id}</td>
+                      <td>{announcement.message}</td>
+                      <td>{announcement.status}</td>
+                      <td>{announcement.updatedAt}</td>
+                      <td><button data-toggle="modal" data-target="#announcement-editor" onClick={()=> this.populateModal.call(this,announcement)}>edit</button></td>
+                      <td>deactivate</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
