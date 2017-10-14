6f36a89a043aacdb73378c1ceeec6d7c89cd2a8a
Shweta Patrachari <shwetachari@gmail.com>
2017-10-13 18:38:25 -0700

    added request to get travel time

--- client/src/components/customer/GMap.jsx
+++ client/src/components/customer/GMap.jsx
@@ -9,10 +9,6 @@ class GMap extends React.Component {
     super(props);
   }
 
-  componentDidMount() {
-
-  }
-
   componentWillReceiveProps({ isScriptLoadSucceed }) {
     if (isScriptLoadSucceed) {
       this.map = new google.maps.Map(this.refs.map, {
@@ -46,34 +42,6 @@ class GMap extends React.Component {
     );
   }
 };
-
-// const Marker = (props) => (
-//   <div >
-//     <div style={{width: '30px', height: '30px', fontSize: '10px', paddingTop:'10px', background: 'skyblue', borderRadius: '50%', textTransform: 'uppercase', color: 'white', textAlign: 'center'}}>You</div>
-//   </div>
-// );
-//
-// const Restaurant = (props) => (
-//   <div>
-//     <img style={{width: '30px', height: '30px', transform: 'translate(0, -15px)'}} src={balloon}/>
-//   </div>
-// );
-//
-// const GMap = (props) => (
-//   <GoogleMapReact style={{width: '250px', height: '250px', position: 'relative'}}
-//     size={{width: '250px', height: '250px'}}
-//     bootstrapURLKeys={{key: props.apiKey}}
-//     center={{lat: !!props.restaurant ? props.restaurant.latitude : (!!props.location ? props.location.latitude : 37.7837625), lng: !!props.restaurant ? props.restaurant.longitude : (!!props.location ? props.location.longitude : -122.4090708)}}
-//     defaultZoom={13}
-//     minZoom={4}>
-//     { props.you ?
-//       <Marker lat={props.location.latitude} lng={props.location.longitude}/>
-//       : ''}
-//     { props.isMarkerShown ?
-//       <Restaurant lat={props.restaurant.latitude} lng={props.restaurant.longitude} />
-//       : '' }
-//   </GoogleMapReact>
-// );
 
 export default scriptLoader(
   [`https://maps.googleapis.com/maps/api/js?key=${api_key}`]
